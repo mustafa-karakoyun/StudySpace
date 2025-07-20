d021bdf056846619c02649ab745592630a723d9f
mustafa-karakoyun <karakoyunmustafa739@gmail.com>
2025-07-20 21:22:43 +0300

    Güncelleme: Galeri/müzik/değişiklik

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Music } from 'lucide-react'
+
 import { MusicProvider } from './context/MusicContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
